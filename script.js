caf313837af0a138e55c2ab1da117e198c407cb1
Racoma10 <ruelracoma09@gmail.com>
2024-09-06 10:47:13 +0800

    added activity

---
+++ script.js
@@ -0,0 +1,8 @@
+let firstname = "Raymart";
+let lastname = "Avancena";
+
+console.log("Hi I'm " + firstname + " " + lastname + " 2nd year Bsit student" );
+alert("Hi I'm" + firstname + " " + lastname + "2nd year BSIT student" );
+
+document.getElementById("Text").innerHTML= "Hi I'm " + firstname + " " + lastname + " 2nd year BSIT student";
+
